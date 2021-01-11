0e45f9a9a40106d4b9f83249ca0ba5afa406903e
xiaoxiaoxiannv <1017929760@qq.com>
2021-01-11 17:52:40 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
 
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000
+
+axios.interceptors.response.use(function (response){
+  let result = response.data;
+  if(result.status === 0){
+    return result.data
+  }else if(result.status === 10){
+    window.location.href = '/#/login'
+  }else{
+    alert(result.message)
+  }
+})
+//Vue.use(axios)
 Vue.config.productionTip = false
 
 new Vue({
