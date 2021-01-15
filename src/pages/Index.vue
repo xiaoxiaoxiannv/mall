a90f59dce63b6f8cc40b554915566a8d03fcf6c8
xiaoxiaoxiannv <1017929760@qq.com>
2021-01-15 15:15:41 +0800

    轮播部分菜单栏css

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,42 @@
   <div class="index">
     <div class="container">
       <div class="swiper-box">
+        <div class="nav-menu">
+          <ul class="menu-wrapper">
+            <li class="menu-item">
+              <a href="javascript:;">手机 电话卡</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电视 盒子</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">笔记本 平板</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">家电 插线板</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">出行 穿戴</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">耳机 音箱</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">电源 配件</a>
+              <div class="children"></div>
+            </li>
+            <li class="menu-item">
+              <a href="javascript:;">生活 箱包</a>
+              <div class="children"></div>
+            </li>
+          </ul>
+        </div>
         <swiper :options="swiperOption">
           <swiper-slide v-for="(item,index) in slideList" :key="index">
             <a :href="'/#/product/'+item.id">
@@ -50,38 +86,82 @@ export default {
           prevEl: '.swiper-button-prev',
         }
       },
-        slideList: [
-          {
-            id: '42',
-            img: '/imgs/slider/slide-1.jpg'
-          },
-          {
-            id: '45',
-            img: '/imgs/slider/slide-2.jpg'
-          },
-          {
-            id: '46',
-            img: '/imgs/slider/slide-3.jpg'
-          },
-          {
-            id: '',
-            img: '/imgs/slider/slide-4.jpg'
-          },
-          {
-            id: '',
-            img: '/imgs/slider/slide-1.jpg'
-          }
-        ]
-      }
+      slideList: [
+        {
+          id: '42',
+          img: '/imgs/slider/slide-1.jpg'
+        },
+        {
+          id: '45',
+          img: '/imgs/slider/slide-2.jpg'
+        },
+        {
+          id: '46',
+          img: '/imgs/slider/slide-3.jpg'
+        },
+        {
+          id: '',
+          img: '/imgs/slider/slide-4.jpg'
+        },
+        {
+          id: '',
+          img: '/imgs/slider/slide-1.jpg'
+        }
+      ]
     }
   }
+}
 </script>
 
 <style lang="scss">
+@import "./../assets/scss/mixin";
+@import "./../assets/scss/config";
+
 .index {
   .swiper-box {
+    position: relative;
+
+    .nav-menu {
+      position: absolute;
+      z-index: 9;
+      width: 264px;
+      height: 451px;
+      background: #55585a7a;
+      box-sizing: border-box;
+      padding: 26px 0;
+
+      .menu-wrapper {
+        .menu-item {
+          height: 50px;
+          line-height: 50px;
+
+          a {
+            display: block;
+            position: relative;
+            padding-left: 30px;
+            font-size: 16px;
+            color: white;
+
+            &:after {
+              position: absolute;
+              right: 30px;
+              top: 17.5px;
+              content: ' ';
+              @include bgImg(10px, 15px, '/imgs/icon-arrow.png');
+            }
+          }
+          &:hover{
+            background: $colorA;
+          };
+        }
+      }
+    }
+
     .swiper-container {
       height: 451px;
+      .swiper-button-prev{
+        left: 274px;
+      }
 
       img {
         width: 100%;
