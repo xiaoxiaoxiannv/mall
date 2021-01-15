8ee09a636d4841c54b3596f70e59a9b42fee1713
xiaoxiaoxiannv <1017929760@qq.com>
2021-01-15 16:17:30 +0800

    轮播分级菜单栏实现

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,35 +6,36 @@
           <ul class="menu-wrapper">
             <li class="menu-item">
               <a href="javascript:;">手机 电话卡</a>
-              <div class="children"></div>
+              <div class="children">
+                <ul v-for="(item,i) in menuList" :key="i">
+                  <li v-for="(sub,j) in item" :key="j">
+                    <a :href="sub? '/#/product/' +sub.id : ''">
+                      <img :src="sub? sub.img: '/imgs/item-box-1.png'">
+                      {{ sub ? sub.name : '小米9' }}
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">电视 盒子</a>
-              <div class="children"></div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">笔记本 平板</a>
-              <div class="children"></div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">家电 插线板</a>
-              <div class="children"></div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">出行 穿戴</a>
-              <div class="children"></div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">耳机 音箱</a>
-              <div class="children"></div>
             </li>
             <li class="menu-item">
-              <a href="javascript:;">电源 配件</a>
-              <div class="children"></div>
-            </li>
+              <a href="javascript:;">电源 配件</a></li>
             <li class="menu-item">
               <a href="javascript:;">生活 箱包</a>
-              <div class="children"></div>
             </li>
           </ul>
         </div>
@@ -107,6 +108,31 @@ export default {
           id: '',
           img: '/imgs/slider/slide-1.jpg'
         }
+      ],
+      menuList: [
+        [
+          {
+            id: 30,
+            img: '/imgs/item-box-1.png',
+            name: '小米CC9'
+          },
+          {
+            id: 31,
+            img: '/imgs/item-box-2.png',
+            name: '小米8青春版',
+          },
+          {
+            id: 32,
+            img: '/imgs/item-box-3.jpg',
+            name: 'Redmi K20 Pro',
+          },
+          {
+            id: 33,
+            img: '/imgs/item-box-4.jpg',
+            name: '移动4G专区',
+          }
+        ],
+        [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]
       ]
     }
   }
@@ -150,16 +176,57 @@ export default {
               @include bgImg(10px, 15px, '/imgs/icon-arrow.png');
             }
           }
-          &:hover{
+
+          &:hover {
             background: $colorA;
-          };
+            .children{
+              display: block;
+            }
+          }
+
+          .children {
+            display: none;
+            position: absolute;
+            border: 1px solid $colorH;
+            height: 451px;
+            width: 946px;
+            background: $colorG;
+            top: 0;
+            left: 264px;
+
+            ul {
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+
+              li {
+                height: 75px;
+                line-height: 75px;
+                flex: 1;
+                padding-left: 23px;
+              }
+
+              a {
+                color: $colorB;
+                font-size: 14px;
+              }
+
+              img {
+                width: 42px;
+                height: 35px;
+                vertical-align: middle;
+                margin-right: 15px;
+              }
+            }
+          }
         }
       }
     }
 
     .swiper-container {
       height: 451px;
-      .swiper-button-prev{
+
+      .swiper-button-prev {
         left: 274px;
       }
 
