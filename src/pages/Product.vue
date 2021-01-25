2a4ce6b992333d67f797dc11da73e37d6cc1d2fd
xiaoxiaoxiannv <1017929760@qq.com>
2021-01-25 16:42:09 +0800

    视频模块slide动画

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -48,11 +48,11 @@
       <div class="item-video">
         <h2>60帧超慢动作摄影 慢慢回味每一瞬间的精彩</h2>
         <p>后置960帧电影般超慢动作视频，将眨眼间的美妙展现得淋漓尽致！ 更能AI 精准分析视频内容，15个场景智能匹配背景音效。</p>
-        <div class="video-bg"></div>
+        <div class="video-bg" @click="showSlide=true"></div>
         <div class="video-box">
-          <div class="overlay"></div>
-          <div class="video">
-            <span class="icon-close"></span>
+          <div class="overlay" v-if="showSlide"></div>
+          <div class="video" :class="{'slide':showSlide}">
+            <span class="icon-close" @click="showSlide=false"></span>
             <video src="/imgs/product/video.mp4" muted autoplay controls="controls"></video>
           </div>
         </div>
@@ -70,6 +70,7 @@ export default {
   components: {ProductParam,Swiper,SwiperSlide},
   data(){
     return{
+      showSlide:false,
       swiperOption:{
         autoplay:true,
         slidesPerView:3,
@@ -174,12 +175,18 @@ export default {
         }
         .video{
           position:fixed;
-          top:50%;
+          top:-50%;
           left:50%;
           transform:translate(-50%,-50%);
           z-index:10;
           width:1000px;
           height:536px;
+          opacity: 0;
+          transition: all 600ms;
+          &.slide{
+            top: 50%;
+            opacity: 1;
+          }
           .icon-close{
             position:absolute;
             top:20px;
