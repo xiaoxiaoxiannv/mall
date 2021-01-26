143101b8de81e6778762ed1159664c2f36ca7737
xiaoxiaoxiannv <1017929760@qq.com>
2021-01-26 18:50:32 +0800

    购物车页面样式实现

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -11,17 +11,21 @@
         </h2>
       </div>
       <div class="username">
-        <a href="javascript">admin</a>
+        <a href="javascript">{{username}}</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 export default {
   name: "OrderHeader",
   props:{
     title:String
+  },
+  computed:{
+    ...mapState(['username'])
   }
 }
 </script>
