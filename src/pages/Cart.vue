e4d3b55e93aebe55c88107227f5efdab25ea8d79
xiaoxiaoxiannv <1017929760@qq.com>
2021-01-27 19:03:42 +0800

    购物车商品渲染

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,85 +1,108 @@
 <template>
-<div class="cart">
-  <OrderHeader title="我的购物车">
-    <template v-slot:tip>
-      <span>温馨提示：产品是否购买成功，以最终下单为准哦，请尽快结算</span>
-    </template>
-  </OrderHeader>
-  <div class="wrapper">
-    <div class="container">
-      <div class="cart-box">
-        <ul class="cart-item-head">
-          <li class="col-1">
-            <span class="checkbox checked"></span>
-            全选
-          </li>
-          <li class="col-3">商品名称</li>
-          <li class="col-1">单价</li>
-          <li class="col-2">数量</li>
-          <li class="col-1">小计</li>
-          <li class="col-1">操作</li>
-        </ul>
-        <ul class="cart-item-list">
-          <li class="cart-item">
-            <div class="item-check">
-              <span class="checkbox checked"></span>
-            </div>
-            <div class="item-name">
-              <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/ca9b4f81af709935556bef9aa21a90e8.jpg">
-              <span>Redmi Note 8 千元4800万四摄</span>
-            </div>
-            <div class="item-price">999</div>
-            <div class="item-num">
-              <div class="num-box">
-                <a href="javascript:;">-</a>
-                <span>1</span>
-                <a href="javascript:;">+</a>
+  <div class="cart">
+    <order-header title="我的购物车">
+      <template v-slot:tip>
+        <span>温馨提示：产品是否购买成功，以最终下单为准哦，请尽快结算</span>
+      </template>
+    </order-header>
+    <div class="wrapper">
+      <div class="container">
+        <div class="cart-box">
+          <ul class="cart-item-head">
+            <li class="col-1">
+              <span class="checkbox" :class="{'checked':allChecked}"></span>全选
+            </li>
+            <li class="col-3">商品名称</li>
+            <li class="col-1">单价</li>
+            <li class="col-2">数量</li>
+            <li class="col-1">小计</li>
+            <li class="col-1">操作</li>
+          </ul>
+          <ul class="cart-item-list">
+            <li class="cart-item" v-for="(item,index) in list" :key="index">
+              <div class="item-check">
+                <span class="checkbox" :class="{'checked':item.productSelected}"></span>
               </div>
-            </div>
-            <div class="item-total">1</div>
-            <div class="item-del"></div>
-          </li>
-        </ul>
-      </div>
-      <div class="order-wrap clearfix">
-        <div class="cart-tip fl">
-          <a href="/#/index">继续购物</a>
-          共<span>3</span>件商品,已选择<span>1</span>件
+              <div class="item-name">
+                <img v-lazy="item.productMainImage" alt="">
+                <span>{{ item.productName + ' , ' + item.productSubtitle }}</span>
+              </div>
+              <div class="item-price">{{ item.productPrice }}</div>
+              <div class="item-num">
+                <div class="num-box">
+                  <a href="javascript:;">-</a>
+                  <span>{{ item.quantity }}</span>
+                  <a href="javascript:;">+</a>
+                </div>
+              </div>
+              <div class="item-total">{{ item.productTotalPrice }}</div>
+              <div class="item-del"></div>
+            </li>
+          </ul>
         </div>
-        <div class="total fr">
-          合计: <span>999</span>元
-          <a href="javascript" class="btn">去结算</a>
+        <div class="order-wrap clearfix">
+          <div class="cart-tip fl">
+            <a href="/#/index">继续购物</a>
+            共<span>{{ list.length }}</span>件商品，已选择<span>{{ checkedNum }}</span>件
+          </div>
+          <div class="total fr">
+            合计：<span>{{ cartTotalPrice }}</span>元
+            <a href="javascript:;" class="btn">去结算</a>
+          </div>
         </div>
       </div>
     </div>
+    <service-bar></service-bar>
+    <nav-footer></nav-footer>
   </div>
-  <ServiceBar/>
-  <NavFooter/>
-</div>
 </template>
 
 <script>
 import OrderHeader from "@/components/OrderHeader";
 import NavFooter from "@/components/NavFooter";
 import ServiceBar from "@/components/ServiceBar";
+
 export default {
   name: "cart",
-  components: {NavFooter, OrderHeader,ServiceBar}
+  components: {NavFooter, OrderHeader, ServiceBar},
+  data() {
+    return {
+      list: [],//商品列表
+      allChecked: false,//是否全选
+      cartTotalPrice: 0,//商品总金额
+      checkedNum: 0//选中商品数量
+    }
+  },
+  mounted() {
+    this.getCartList();
+  },
+  methods: {
+    getCartList() {
+      this.axios.get('/carts').then((res) => {
+        this.list = res.cartProductVoList || [];
+        this.allChecked = res.selectedAll;
+        this.cartTotalPrice = res.cartTotalPrice;
+        this.checkedNum = this.list.filter(item => item.productSelected).length;
+      })
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-.cart{
-  .wrapper{
+.cart {
+  .wrapper {
     background: #F5F5F5;
     padding-top: 30px;
     padding-bottom: 114px;
-    .cart-box{
+
+    .cart-box {
       background: #ffffff;
       font-size: 14px;
       text-align: center;
       color: #666666;
-      .checkbox{
+
+      .checkbox {
         display: inline-block;
         width: 22px;
         height: 22px;
@@ -87,81 +110,98 @@ export default {
         vertical-align: middle;
         margin-right: 17px;
         cursor: pointer;
-        &.checked{
+
+        &.checked {
           background: url("/imgs/icon-gou.png") #FF6600 no-repeat center;
           background-size: 16px 12px;
           border: none;
         }
       }
-      .cart-item-head{
+
+      .cart-item-head {
         display: flex;
         height: 79px;
         line-height: 79px;
-        .col-1{
+
+        .col-1 {
           flex: 1;
         }
-        .col-2{
+
+        .col-2 {
           flex: 2;
         }
-        .col-3{
+
+        .col-3 {
           flex: 3;
         }
       }
-      .cart-item-list{
-        .cart-item{
+
+      .cart-item-list {
+        .cart-item {
           display: flex;
           align-items: center;
           height: 125px;
           border-top: 1px solid #e5e5e5;
           font-size: 16px;
-          .item-check{
+
+          .item-check {
             flex: 1;
           }
-          .item-name{
+
+          .item-name {
             flex: 3;
             font-size: 18px;
             color: #333333;
             display: flex;
             align-items: center;
-            img{
+
+            img {
               width: 80px;
               height: 80px;
               vertical-align: center;
             }
-            span{
+
+            span {
               margin-left: 30px;
             }
           }
-          .item-price{
+
+          .item-price {
             flex: 1;
             color: #333333;
           }
-          .item-num{
+
+          .item-num {
             flex: 2;
-            .num-box{
+
+            .num-box {
               display: inline-block;
               width: 150px;
               height: 40px;
               line-height: 40px;
               border: 1px solid #e5e5e5;
               font-size: 14px;
-              a{
+
+              a {
                 display: inline-block;
                 width: 50px;
                 color: #333333;
               }
-              span{
+
+              span {
                 display: inline-block;
                 width: 50px;
                 color: #333333;
               }
             }
           }
-          .item-total{
+
+          .item-total {
             flex: 1;
             color: #FF6600;
           }
-          .item-del{
+
+          .item-del {
             flex: 1;
             width: 14px;
             height: 12px;
@@ -172,30 +212,37 @@ export default {
         }
       }
     }
-    .order-wrap{
+
+    .order-wrap {
       font-size: 14px;
       color: #666666;
       margin-top: 20px;
       height: 50px;
       line-height: 50px;
-      .cart-tip{
+
+      .cart-tip {
         margin-left: 29px;
-        a{
+
+        a {
           color: #666666;
           margin-right: 37px;
         }
-        span{
+
+        span {
           color: #FF6600;
           margin: 0 5px;
         }
       }
-      .total{
+
+      .total {
         font-size: 14px;
         color: #FF6600;
-        span{
+
+        span {
           font-size: 24px;
         }
-        a{
+
+        a {
           width: 202px;
           height: 50px;
           line-height: 50px;
