8040c7d63cdd209c4741e860ab7fb8a6d1e25aed
xiaoxiaoxiannv <1017929760@qq.com>
2021-01-29 16:53:11 +0800

    order结构封装

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -1,17 +1,44 @@
 <template>
-<div>
-  <OrderHeader/>
-  <router-view></router-view>
-  <NavFooter/>
-</div>
+  <div>
+    <OrderHeader :title="title">
+      <template v-slot:tip>
+        <span>{{ tip }}</span>
+      </template>
+    </OrderHeader>
+    <router-view></router-view>
+    <ServiceBar></ServiceBar>
+    <NavFooter/>
+  </div>
 </template>
 
 <script>
 import OrderHeader from "@/components/OrderHeader";
 import NavFooter from "@/components/NavFooter";
+import ServiceBar from "@/components/ServiceBar";
+
 export default {
-name: "Order",
-  components: {NavFooter, OrderHeader}
+  name: "Order",
+  data() {
+    return {
+      title: '',
+      tip: ''
+    }
+  },
+  components: {NavFooter, OrderHeader, ServiceBar},
+  mounted() {
+    let path = this.$route.path;
+    if (path === '/order/confirm') {
+      this.title = '订单确认';
+      this.tip = '请认真填写收货地址';
+    } else if (path === '/order/list') {
+      this.title = '订单列表';
+      this.tip = '请谨防钓鱼链接或诈骗电话，了解更多>';
+
+    } else if (path === '/order/pay') {
+      this.title = '订单支付';
+      this.tip = '请谨防钓鱼链接或诈骗电话，了解更多>';
+    }
+  }
 }
 </script>
 
