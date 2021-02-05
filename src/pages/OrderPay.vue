482c0c96602ddd47c0dc93e4618973925d82328a
xiaoxiaoxiannv <1017929760@qq.com>
2021-02-05 16:25:12 +0800

    订单支付页面样式实现

--- src/pages/OrderPay.vue
+++ src/pages/OrderPay.vue
@@ -1,13 +1,212 @@
 <template>
-<div>order-pay</div>
+  <div class="order-pay">
+    <div class="wrapper">
+      <div class="container">
+        <div class="order-wrap">
+          <div class="item-order">
+            <div class="icon-succ"></div>
+            <div class="order-info">
+              <h2>订单提交成功！去付款咯～</h2>
+              <p>请在<span>30分</span>内完成支付, 超时将取消订单</p>
+              <p>收货信息:admin 17600000000 北京 北京市 海淀区 百度科技园</p>
+            </div>
+            <div class="order-total">
+              <p>应付总额:<span>10</span>元</p>
+              <p>订单详情<em class="icon-down"></em></p>
+            </div>
+          </div>
+          <div class="item-detail">
+            <div class="item">
+              <div class="detail-title">订单号:</div>
+              <div class="detail-info theme-color">1612511253465</div>
+            </div>
+            <div class="item">
+              <div class="detail-title">收货信息:</div>
+              <div class="detail-info">周 15089618555 天津 天津 河北 sdfs</div>
+            </div>
+            <div class="item good">
+              <div class="detail-title">商品名称:</div>
+              <div class="detail-info">
+                <ul>
+                  <li>
+                    <img src="">
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="item">
+              <div class="detail-title">发票信息:</div>
+              <div class="detail-info">电子发票 个人</div>
+            </div>
+          </div>
+        </div>
+        <div class="item-pay">
+          <h3>选择一下支付方式付款</h3>
+          <div class="pay-way">
+            <p>支付平台</p>
+            <div class="pay pay-ali"></div>
+            <div class="pay pay-wechat"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "OrderPay"
+  name: "OrderPay"
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+.order-pay {
+  .wrapper {
+    background: #F5F5F5;
+    padding-top: 30px;
+    padding-bottom: 61px;
+
+    .order-wrap {
+      padding: 62px 50px;
+      background: #ffffff;
+      font-size: 14px;
+      margin-bottom: 30px;
+
+      .item-order {
+        display: flex;
+        align-items: center;
+
+        .icon-succ {
+          width: 90px;
+          height: 90px;
+          border-radius: 50%;
+          background: url("/imgs/icon-gou.png") #80c58a no-repeat center;
+          background-size: 60px;
+          margin-right: 40px;
+        }
+
+        .order-info {
+          margin-right: 248px;
+
+          h2 {
+            font-size: 24px;
+            color: #333333;
+            margin-top: 20px;
+          }
+
+          p {
+            color: #666666;
+
+            span {
+              color: #FF6600;
+            }
+          }
+        }
+
+        .order-total {
+          & > p:first-child {
+            margin-bottom: 30px;
+          }
+
+          span {
+            font-size: 28px;
+            color: #FF6600;
+          }
+
+          .icon-down {
+            display: inline-block;
+            width: 14px;
+            height: 10px;
+            background: url("/imgs/icon-down.png") no-repeat center;
+            background-size: contain;
+            margin-left: 9px;
+            transition: all 500ms;
+            cursor: pointer;
+
+            &.up {
+              transform: rotate(180deg);
+            }
+          }
+
+          .icon-up {
+            transform: rotate(180deg);
+          }
+        }
+      }
+
+      .item-detail {
+        border-top: 1px solid #d7d7d7;
+        padding-top: 47px;
+        padding-left: 130px;
+        font-size: 14px;
+        margin-top: 45px;
+
+        .item {
+          margin-bottom: 19px;
+
+          .detail-title {
+            float: left;
+            width: 100px;
+          }
+
+          .detail-info {
+            display: inline-block;
+
+            img {
+              width: 30px;
+              vertical-align: middle;
+            }
+          }
+        }
+      }
+    }
+
+    .item-pay {
+      padding: 26px 50px 72px;
+      background: #ffffff;
+      color: #333333;
+
+      h3 {
+        font-size: 20px;
+        font-weight: 200;
+        color: #333333;
+        padding-bottom: 24px;
+        border-bottom: 1px solid #d7d7d7;
+        margin-bottom: 26px;
+      }
+
+      .pay-way {
+        font-size: 18px;
+
+        .pay {
+          display: inline-block;
+          width: 188px;
+          height: 64px;
+          border: 1px solid #d7d7d7;
+          cursor: pointer;
+
+          &:last-child {
+            margin-left: 20px;
+          }
+
+          &.checked {
+            border: 1px solid #FF6600;
+          }
+        }
+
+        .pay-ali {
+          background: url("/imgs/pay/icon-ali.png") no-repeat center;
+          background-size: 103px 38px;
+          margin-top: 19px;
+        }
+
+        .pay-wechat {
+          background: url('/imgs/pay/icon-wechat.png') no-repeat center;
+          background-size: 103px 38px;
+        }
+      }
+    }
+  }
+}
 
 </style>
